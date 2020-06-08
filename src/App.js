3b6661905ace7db773fb67e3570ab4a8ced2af57
SeanKLiebel <61285232+SeanKLiebel@users.noreply.github.com>
2020-06-08 17:25:15 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router,Switch,Route, Link} from "react-router-dom";
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 import Navbar from './components/pages/Navbar';
